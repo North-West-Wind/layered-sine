e08daeab948ece7e6af41b700c483b44d43d63d6
North-West-Wind <wsyn148@gmail.com>
2023-02-23 21:30:43 +0800

    bigger canvas

--- test/test.ts
+++ test/test.ts
@@ -2,20 +2,20 @@ import { Canvas } from "canvas";
 import * as fs from "fs";
 import { SineWave, FillMode, animateSines, animateSinesFrames, drawSine } from "../lib";
 
-const wave = new SineWave(4, 60, 1, 0, 20);
+const wave = new SineWave(40, 600, 1, 0, 200);
 wave.fill = FillMode.UP;
 wave.color = 0x555555;
-const wave2 = new SineWave(3, 70, 1.5, Math.PI * Math.random(), 30);
+const wave2 = new SineWave(30, 700, 1.5, Math.PI * Math.random(), 300);
 wave2.fill = FillMode.UP;
 wave2.color = 0x888888;
-const wave3 = new SineWave(2.5, 80, 2, Math.PI * Math.random(), 45);
+const wave3 = new SineWave(25, 800, 2, Math.PI * Math.random(), 450);
 wave3.fill = FillMode.UP;
 wave3.color = 0xaaaaaa;
-const wave4 = new SineWave(2, 100, 3, Math.PI * Math.random(), 60);
+const wave4 = new SineWave(20, 1000, 3, Math.PI * Math.random(), 600);
 wave4.fill = FillMode.UP;
 wave4.color = 0xcccccc;
 
-var canvas = new Canvas(100, 100, "image");
+var canvas = new Canvas(1000, 1000, "image");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#eee";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
