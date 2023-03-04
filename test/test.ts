11a1c2dea28f004af3c8f3faa7de36ddcf420c13
North-West-Wind <wsyn148@gmail.com>
2023-03-04 22:36:11 +0800

    bigger test

--- test/test.ts
+++ test/test.ts
@@ -5,17 +5,17 @@ import { SineWave, FillMode, animateSines, animateSinesFrames, drawSine } from "
 const wave = new SineWave(40, 600, 1, 0, 200);
 wave.fill = FillMode.UP;
 wave.color = 0x555555;
-const wave2 = new SineWave(30, 700, 1.5, Math.PI * Math.random(), 300);
+const wave2 = new SineWave(30, 700, 1.5, Math.PI * Math.random(), 400);
 wave2.fill = FillMode.UP;
 wave2.color = 0x888888;
-const wave3 = new SineWave(25, 800, 2, Math.PI * Math.random(), 450);
+const wave3 = new SineWave(25, 800, 2, Math.PI * Math.random(), 600);
 wave3.fill = FillMode.UP;
 wave3.color = 0xaaaaaa;
-const wave4 = new SineWave(20, 1000, 3, Math.PI * Math.random(), 600);
+const wave4 = new SineWave(20, 1000, 3, Math.PI * Math.random(), 900);
 wave4.fill = FillMode.UP;
 wave4.color = 0xcccccc;
 
-var canvas = new Canvas(1000, 1000, "image");
+var canvas = new Canvas(1920, 1080, "image");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#eee";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
